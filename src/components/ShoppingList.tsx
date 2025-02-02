4f36c0f6c27311cb2e454056ebece31089cb8ff6
vh-mendes <vitorhugomendes995@gmail.com>
2025-02-01 21:13:58 -0300

    Adicionando funcionalidades na lista de compra

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Alert from "@mui/material/Alert";
 
-export function ShoppingList() {
-  const [items, setItems] = useState<{ nome: string; quantidade: number; }[]>(
+export function ShoppingList(){ 
+  const [items, setItems] = useState<{ id: number; nome: string; quantidade: number; comprado: boolean; }[]>(
     []
   ); //Armazeno os itens da lista
   const [newItem, setNewItem] = useState<string>("");
@@ -10,13 +10,42 @@ export function ShoppingList() {
   const [error, setError] = useState<string>("");
   const [sucess, setSucess] = useState<string>("");
 
-  const RemoveItem = (index: number) => {   
-    setItems(items.filter((_, i) => i !== index)); //Remove o item da lista
-  }
-  const RemoveAllList = () => {
-     //Remove todos os itens da lista
-    setItems([]);
+  const RemoveItem = (id: number) => {
+    setItems(items.filter((item) => item.id !== id)); //Remove o item da lista
+  };
+
+  const itemsNaoComprados = items.filter((item) => !item.comprado); //Filtra os itens não comprados
+  const itemsComprados = items.filter((item) => item.comprado); //Filtra os itens comprados
+
+  const RemoveList = () => {
+    setError("");
+    setSucess("");
+    //Remove todos os itens da lista de não comprados
+      if (itemsNaoComprados.length > 0) {
+        setItems(items.filter((item) => item.comprado)); 
+      } else { //Se não houver itens não comprados, exibe o erro
+        setError("Não há itens na lista para remover");
+        //setTimeout(() => setError(""), 2000); // Limpa a mensagem de erro após 3 segundos
+      }
+  };
+
+  const RemoveListComprado = () => {
+    setError("");
+    setSucess("");
+    //Remove todos os itens da lista de não comprados
+    if (itemsComprados.length > 0) {
+      setItems(items.filter((item) => !item.comprado));
+    } else { //Se não houver itens comprados, exibe o erro
+      setError("Não há itens na lista para remover");
+      //setTimeout(() => setError(""), 2000); // Limpa a mensagem de erro após 3 segundos
     }
+  };
+
+  const ItemComprado = (id: number) => { 
+    setItems(items.map(item  => 
+      item.id === id ? { ...item, comprado: !item.comprado } : item
+  )); //Marca o item como comprado
+};
 
   const AddItem = () => {
     //Reseta os alertas antes de qualquer coisa
@@ -38,7 +67,7 @@ export function ShoppingList() {
       //setTimeout(() => setError(""), 2000); // Limpa a mensagem de erro após 3 segundos
       return; //Se a quantidade for negativa, não adiciona e aborta
     }
-    setItems([...items, { nome: newItem, quantidade: newQuantity }]); //Adiciona o item na lista
+    setItems([...items, { id: Date.now(), nome: newItem, quantidade: newQuantity, comprado: false }]); //Adiciona o item na lista
     setNewItem(""); //Reseta o campo de nome para o padrão
     setNewQuantity(1); //Reseta o campo de quantidade para o padrão
     setSucess("Item adicionado com sucesso"); //Reseta o campo de erro para o padrão
@@ -61,18 +90,34 @@ export function ShoppingList() {
         type="number"
         value={newQuantity}
         onChange={(e) => setNewQuantity(Number(e.target.value))}
-        placeholder="Digite a quantidade"
+        min="1"
       />
-      <button onClick={AddItem}>Adicionar</button>  
-      <button onClick={RemoveAllList}>Limpar Lista</button>
+      <button onClick={AddItem}>Adicionar</button>
+      <h2>Itens na Lista</h2><button onClick={RemoveList}>Limpar Lista</button>
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>
+        {itemsNaoComprados.map((item, id) => (
+          <li key={id}>
             {item.nome} - Quantidade: {item.quantidade}
-            <button onClick={() => RemoveItem(index)}>Remover</button> {/* Passa o índice correto */}
-            </li>
+            <button onClick={() => RemoveItem(item.id)}>Remover</button>{" "}
+            <button onClick={() => ItemComprado(item.id)}>
+            {item.comprado ? "Desmarcar" : "Marcar como Comprado"}
+            </button>
+            {/* Passa o índice correto */}
+          </li>
         ))}
       </ul>
+
+      <h2>Itens Comprados</h2><button onClick={RemoveListComprado}>Limpar Lista</button>
+      <ul>
+        {itemsComprados.map((item, id) => (
+          <li key={id}>
+            {item.nome} - Quantidade: {item.quantidade}
+            <button onClick={() => RemoveItem(item.id)}>Remover</button>{" "}
+            <button onClick={() => ItemComprado(item.id)}>{item.comprado ? "Desmarcar" : "Marcar como Comprado"}</button>
+            </li>
+        ))}
+      </ul>          
     </div>
   );
 }
+
